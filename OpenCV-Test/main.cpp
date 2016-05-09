2bdac6200799931a7ab1fbdfe3e99108f93dca0a
Andriy Bas <andriy.bas@uptech.team>
2016-05-09 22:21:42 +0300

    Initial Commit

---
+++ OpenCV-Test/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OpenCV-Test
+//
+//  Created by Andriy Bas on 5/9/16.
+//  Copyright © 2016 Andriy Bas. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
